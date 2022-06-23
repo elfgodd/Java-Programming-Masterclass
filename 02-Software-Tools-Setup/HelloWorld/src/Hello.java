5eb44f5ece9b7a10e7a71eff60eebfa244ddfbd6
ElfGod <albertogx@icloud.com>
2022-06-22 23:20:48 -0500

    Starting out with Expressions

--- 02-Software-Tools-Setup/HelloWorld/src/Hello.java
+++ 02-Software-Tools-Setup/HelloWorld/src/Hello.java
@@ -14,9 +14,18 @@ public class Hello {
     System.out.println(multi2Nums); // 27.5
 
     System.out.println("\n");
-    int operations2Nums = (10 + 40) + (2 * 20);
+    int operations2Nums = (10 + 40) + (2 * 20); // 90
     System.out.println(operations2Nums);
     
-    
+    System.out.println("\n");
+    int mySecondNumber = 12;
+    int myThirdNumber = myFirstNumber * 2;
+    int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+    System.out.println(myTotal); // 27
+
+    System.out.println("\n");
+    int myLastOne = 1000 - myTotal;
+    System.out.println(myLastOne); // 973
+
   }
 }
