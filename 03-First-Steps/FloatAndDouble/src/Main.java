2f8f2ab116c8716f41c79bf43ba892deb211b1a6
ElfGod <albertogx@icloud.com>
2022-06-28 23:33:09 -0500

    Floating Point Precision and a Challenge

--- 03-First-Steps/FloatAndDouble/src/Main.java
+++ 03-First-Steps/FloatAndDouble/src/Main.java
@@ -1,3 +1,5 @@
+import javax.sound.sampled.SourceDataLine;
+
 public class Main {
     public static void main(String[] args) {
         
@@ -17,7 +19,49 @@ public class Main {
         
         // Long way to convert float to float
         // float myFloatValue = (float) 5.25;
+        System.out.println("\n");
         float myFloatValue = 5.25f;
         double myDoubleValue = 5.25d;
+        System.out.println("float myFloatValue = 5.25f " + myFloatValue);
+        System.out.println("double myDoubleValue = 5.25d " + myDoubleValue);
+
+        System.out.println("\n");
+        int myIntValue2 = 5;
+        float myFloatValue2 = 5f;
+        double myDoubleValue2 = 5d;
+        System.out.println("MyIntValue2 = " + myIntValue2);
+        System.out.println("MyFloatValue2 = " + myFloatValue2);
+        System.out.println("MyDoubleValue2 = " + myDoubleValue2);
+
+        System.out.println("\n");
+        myIntValue2 = 5 / 2;
+        myFloatValue2 = 5f / 2;
+        myDoubleValue2 = 5d / 2;
+        System.out.println("MyIntValue2 = " + myIntValue2);
+        System.out.println("MyFloatValue2 = " + myFloatValue2);
+        System.out.println("MyDoubleValue2 = " + myDoubleValue2);
+
+        System.out.println("\n");
+        myIntValue2 = 5 / 3;
+        myFloatValue2 = 5f / 3;
+        myDoubleValue2 = 5d / 3;
+        System.out.println("MyIntValue2 = " + myIntValue2);
+        System.out.println("MyFloatValue2 = " + myFloatValue2);
+        System.out.println("MyDoubleValue2 = " + myDoubleValue2);
+
+        System.out.println("\n");
+        myDoubleValue2 = 5.00 / 3.00;
+        System.out.println("MyDoubleValue2 = " + myDoubleValue2);
+
+        System.out.println("\n");
+        double numberOfPounds = 200d;
+        double convertedKilograms = numberOfPounds * 0.45359237d;
+        System.out.println("Converted kilograms = " + convertedKilograms);
+
+        System.out.println("\n");
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println("pi = " + pi);
+        System.out.println("anotherNumber = " + anotherNumber);
     }
 }
