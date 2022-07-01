85baa1519d8bd11f325ef314eb2138ad962bb9a4
ElfGod <albertogx@icloud.com>
2022-06-30 23:33:22 -0500

    Abbreviating Operators

--- 03-First-Steps/OperatorsOperandsAndExpressions/src/App.java
+++ 03-First-Steps/OperatorsOperandsAndExpressions/src/App.java
@@ -21,5 +21,31 @@ public class App {
         System.out.println("\n");
         result = result % 3; // the remainder of(4 % 3 = 1)
         System.out.println("4 % 3 = " + result);
+
+        System.out.println("\n");
+        // result = result + 1;
+        result++; // 1 + 1 = 2
+        System.out.println("1 + 1 = " + result);
+        // result = result - 1;
+        result--; // 2 - 1 = 1
+        System.out.println("2 - 1 = " + result);
+
+        System.out.println("\n");
+        // result = result + 2;
+        result += 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        // result = result - 2
+        result -= 2; // 3 - 2 = 1
+        System.out.println("3 - 2 = " + result);
+
+        // result = result * 10;
+        result *= 10; // 1 * 10 = 10
+        System.out.println("1 * 10 = " + result);
+
+        // result = result / 3;
+        result /= 3; // 10 / 3 = 10
+        System.out.println("10 / 3 = " + result);
+
     }
 }
