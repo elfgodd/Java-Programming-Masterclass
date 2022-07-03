49e4d4214ca25bfce4301fb6108fb8934f124dcc
ElfGod <albertogx@icloud.com>
2022-07-03 11:54:40 -0500

    if-then Statement

---
+++ 03-First-Steps/IfThenStatement/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        boolean isAlien = false;
+        if (isAlien == false) {
+          System.out.println("It is not an alien!");
+          System.out.println("And i am scared of aliens");
+        }
+    }
+}
