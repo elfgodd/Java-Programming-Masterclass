2a24d86f7b5a000fdc192627d5f12d88f76d7e92
ElfGod <albertogx@icloud.com>
2022-07-04 23:35:45 -0500

    Coding Exercises: SpeedConverter part2

--- 04-Expressions-Statements-Codeblocks/Exercises/SpeedConverter/src/SpeedConverter.java
+++ 04-Expressions-Statements-Codeblocks/Exercises/SpeedConverter/src/SpeedConverter.java
@@ -12,4 +12,15 @@ public class SpeedConverter {
     // Another way to do it
     return Math.round(kilometersPerHour / 1.609);
   }
+
+  public static void printConversion(double kilometersPerHour) {
+    if (kilometersPerHour < 0) {
+      System.out.println("Invalid Value");
+    } else {
+      long milesPerHour = toMilesPerHour(kilometersPerHour);
+      System.out.println(kilometersPerHour +
+          " km/h = " + milesPerHour + " mi/h");
+    }
+
+  }
 }
