072f380adcfa289ed9a86e1117f55cc382c8f574
ElfGod <albertogx@icloud.com>
2022-07-09 21:37:28 -0500

    Method Overloading

---
+++ 04-Expressions-Statements-Codeblocks/MethodOverloading/src/App.java
@@ -0,0 +1,101 @@
+import java.lang.management.ClassLoadingMXBean;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    int newScore = calculateScore("Alberto", 500);
+    System.out.println("\n");
+
+    System.out.println("New score is " + newScore);
+
+    System.out.println("\n");
+    calculateScore(75);
+
+    System.out.println("\n");
+    calculateScore();
+
+    System.out.println("\n");
+
+    calcFeetAndInchesToCentimeters(6, 0);
+    calcFeetAndInchesToCentimeters(7, 5);
+    calcFeetAndInchesToCentimeters(-10, 1);
+    calcFeetAndInchesToCentimeters(0, 1);
+    calcFeetAndInchesToCentimeters(6, -10);
+
+    double centimeters = calcFeetAndInchesToCentimeters(6, 13);
+    if (centimeters < 0.0) {
+      System.out.println("Invalid parameters");
+    }
+
+    System.out.println("\n");
+    calcFeetAndInchesToCentimeters(100);
+    calcFeetAndInchesToCentimeters(157);
+    calcFeetAndInchesToCentimeters(-10);
+
+    // https://www.metric-conversions.org/length/feet-to-centimeters.htm
+
+    // Create a method called calcFeetAndInchesToCentimeters
+    // It needs to have two parameters
+    // feet is the frist parameter, inches is the 2nd parameter
+    //
+    // You should validate that the first parameter feet is >= 0
+    // You should validate that the 2nd parameter inches is >= 0 and <= 12
+    // return -1 from the mthod if either of the above is not true
+    //
+    // If the parameters are valid, then calculate how many centimeters
+    // comprise the feet and inches passed to this method and return
+    // that value.
+    //
+    // Create a 2nd method of the same name but with only one parameter
+    // inches is the parameter
+    // validate that its >= 0
+    // return -1 if it is not true
+    // But if its valid, then calculate how many feet are in the inches
+    // and then here is the tricky part
+    // call the other overloaded method passing the correct feet and inches
+    // calculated so that it can calculate correctly.
+    // hints: Use double for your number datatypes is probably a good idea
+    // 1 inch = 2.54cm and one foot = 12 inches
+    // use the link i give you to confirm your code is calculating correctly.
+    // Calling another overloaded method just requires you to use the
+    // right number of parameters.
+  }
+
+  public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+    if ((feet < 0) || ((inches < 0) || (inches > 12))) {
+      System.out.println("Invalid feet or inches parameters");
+      return -1;
+    }
+
+    double centimeters = (feet * 12) * 2.54;
+    centimeters += inches * 2.54;
+    System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+    return centimeters;
+  }
+
+  public static double calcFeetAndInchesToCentimeters(double inches) {
+    if (inches < 0) {
+      return -1;
+    }
+
+    double feet = (int) inches / 12;
+    double remainingInches = (int) inches % 12;
+    System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches + " inches");
+    return calcFeetAndInchesToCentimeters(feet, remainingInches);
+  }
+
+  public static int calculateScore(String playerName, int score) {
+    System.out.println("Player " + playerName + " scored " + score + " points");
+    return score * 1000;
+  }
+
+  public static int calculateScore(int score) {
+    System.out.println("Unnamed player scored " + score + " points");
+    return score * 1000;
+  }
+
+  public static int calculateScore() {
+    System.out.println("No player name, No player score.");
+    return 0;
+  }
+
+}
