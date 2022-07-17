08cdf0b063f0d84b35d901c22d4d2924e7d5e227
ElfGod <albertogx@icloud.com>
2022-07-17 12:48:29 -0500

    Coding Exercises: First And Last Digit Sum

---
+++ 05-Control-Flow-Statements/Exercises/FirstAndLastDigitSum/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    public static class FirstLastDigitSum {
+
+        public static int sumFirstAndLastDigit(int number) {
+            if (number < 0) {
+                return -1;
+            }
+
+            int lastDigit = number % 10;
+            while (number > 9) {
+                number /= 10;
+            }
+            return lastDigit + number;
+        }
+    }
+}
