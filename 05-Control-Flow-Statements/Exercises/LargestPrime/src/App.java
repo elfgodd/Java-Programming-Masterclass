36e5120dfd558282091d25c97e4b854be6112eee
ElfGod <albertogx@icloud.com>
2022-07-17 21:57:08 -0500

    Coding Exercises: Largest Prime

---
+++ 05-Control-Flow-Statements/Exercises/LargestPrime/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    public static class LargestPrime {
+        public static int getLargestPrime(int number) {
+            if (number < 2) {
+                return -1;
+            }
+            for (int i = 2; i < number; i++) {
+                if ((number % i) == 0) {
+                    number /= i;
+                    i--;
+                }
+            }
+            return number;
+        }
+    }
+}
