ff487981ab8330e299703e2f23e59cfd88731e9a
ElfGod <albertogx@icloud.com>
2022-07-23 00:17:01 -0500

    Inheritance Challenge Part 2

--- 06-OOP-Part-1/InheritanceChallenge/src/Vehicle.java
+++ 06-OOP-Part-1/InheritanceChallenge/src/Vehicle.java
@@ -16,7 +16,7 @@ public class Vehicle {
 
   public void steer(int direction) {
     this.currentDirection += direction;
-    System.out.println("Vehicle.steer(): Steeing at " + currentDirection + " degrees.");
+    System.out.println("Vehicle.steer(): Steering at " + currentDirection + " degrees.");
   }
 
   public void move(int velocity, int direction) {
@@ -29,32 +29,16 @@ public class Vehicle {
     return name;
   }
 
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public String getSize() {
     return size;
-  }
-
-  public void setSize(String size) {
-    this.size = size;
   }
 
   public int getCurrentVelocity() {
     return currentVelocity;
   }
 
-  public void setCurrentVelocity(int currentVelocity) {
-    this.currentVelocity = currentVelocity;
-  }
-
   public int getCurrentDirection() {
     return currentDirection;
-  }
-
-  public void setCurrentDirection(int currentDirection) {
-    this.currentDirection = currentDirection;
   }
 
   public void stop() {
