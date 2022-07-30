b108792b991aa6fada5531c2aa87ef6bbd599404
ElfGod <albertogx@icloud.com>
2022-07-29 21:02:11 -0500

    98. OOP Challenge - Solution

--- 07-OOP-Part-2/OOPMasterChallengeExercise/src/Hamburger.java
+++ 07-OOP-Part-2/OOPMasterChallengeExercise/src/Hamburger.java
@@ -45,7 +45,7 @@ public class Hamburger {
 
   public double itemizeHamburger() {
     double hamburgerPrice = this.price;
-    System.out.println(this.name + " hamburger " + " on a " + this.breadRollType + " roll "
+    System.out.println(this.name + " hamburger " + "on a " + this.breadRollType + " roll "
         + "with " + this.meat + ", price is " + this.price);
     if (this.addition1Name != null) {
       hamburgerPrice += this.addition1Price;
