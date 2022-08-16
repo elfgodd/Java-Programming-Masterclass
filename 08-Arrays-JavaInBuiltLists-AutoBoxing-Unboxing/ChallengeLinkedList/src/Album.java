8d6f06afd750a5644a04e2d0604cedfcf52751d6
ElfGod <albertogx@icloud.com>
2022-08-16 17:09:25 -0500

    LinkedList Challenge Part 2

--- 08-Arrays-JavaInBuiltLists-AutoBoxing-Unboxing/ChallengeLinkedList/src/Album.java
+++ 08-Arrays-JavaInBuiltLists-AutoBoxing-Unboxing/ChallengeLinkedList/src/Album.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Album {
   private String name;
   private String artist;
@@ -17,26 +20,26 @@ public class Album {
     return false;
   }
 
-  private findSong(String title) {
-    for(Song checkedSong: this.songs) {
-      if(checkedSong.getTitle().equals(title)) {
+  private Song findSong(String title) {
+    for (Song checkedSong : this.songs) {
+      if (checkedSong.getTitle().equals(title)) {
         return checkedSong;
       }
     }
     return null;
   }
 
-  public boolean addToPlayList(int trackNumber, LinkedList<Song> playlist) {
+  public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
     int index = trackNumber - 1;
     if ((index >= 0) && (index <= this.songs.size())) {
-      playlist.add(this.songs.get(index));
+      playList.add(this.songs.get(index));
       return true;
     }
     System.out.println("This album does not have a track " + trackNumber);
     return false;
   }
 
-  public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+  public boolean addToPlayList(String title, LinkedList<Song> playList) {
     Song checkedSong = findSong(title);
     if (checkedSong != null) {
       playList.add(checkedSong);
