d0ac7ca559c26d818aa1c5d01e2d1d23514c8972
ElfGod <albertogx@icloud.com>
2022-08-26 00:44:00 -0500

    Inner Classes Part 2

--- 09-Inner-AbstractClasses-Interfaces/InnerClasses/src/Gearbox.java
+++ 09-Inner-AbstractClasses-Interfaces/InnerClasses/src/Gearbox.java
@@ -11,6 +11,10 @@ public class Gearbox {
     this.gears = new ArrayList<>();
     Gear neutral = new Gear(0, 0.0);
     this.gears.add(neutral);
+
+    for (int i = 0; i < maxGears; i++) {
+      addGear(i, i * 5.3);
+    }
   }
 
   public void operateClutch(boolean in) {
@@ -59,5 +63,4 @@ public class Gearbox {
       return revs * (this.ratio);
     }
   }
-
 }
